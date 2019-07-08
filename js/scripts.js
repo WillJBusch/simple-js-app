61c55a4f0760acdbcc0021b7d0a95164254174ac
WillJBusch <50707325+WillJBusch@users.noreply.github.com>
2019-07-08 17:44:44 +0200

    changed/scripts.js
    deleted everything to start from scratch

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-alert('Hello World!');
+/*var myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+var simpleAddition = 2 + 2;
+document.write(simpleAddition);
+var size = 100;
+var doublesize = size * 2;
+var minisize = (doublesize * 2) - (size / 2);
+document.write(minisize);*/
+var randomNumber = (Math.random() * 6);
+var diceRoll = Math.round(randomNumber);
+document.write(diceRoll);
+
+
+
+
