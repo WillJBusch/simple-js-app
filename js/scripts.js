aa5b0f6a92f234b53ecd356e56d27d16f21284a1
WillJBusch <50707325+WillJBusch@users.noreply.github.com>
2019-07-12 12:23:03 +0200

    changed/scripts.js
    adding another repository-pokemon array
    creating/declaring function with neutral input
    calling function with diffrent repositories

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
-var repository = [
+//Pokemon objects
+var repository = [ 
     {name: 'Bisasam', height: 0.7, type: ['Pflanze', 'Gift']},
     {name: 'Glumanda', height: 0.6, type: ['Feuer']},
     {name: 'Schiggy', height: 0.5, type: ['Wasser']}
 ];
-for (var i = 0; i < repository.length; i++) {
-    if (repository[i].height < 0.7){
-        document.write(repository[i].name +' '+ '(height:' + repository[i].height +')' +' '+'type:'+' ' + repository[i].type + '<br>');
-    } else {
-        document.write(repository[i].name +' '+ '(height:' + repository[i].height +') -Wow, that\'s big! '+'type:'+' ' + repository[i].type + '<br>');
+var repository2 = [
+    {name: 'Raupy', height: 0.3, type: ['Käfer']},
+    {name: 'Hornliu', height: 0.3, type: ['Käfer, Gift']},
+    {name: 'Taubsi', height: 0.3, type: ['Normal, Flug']}
+]
+//declaring function of repository to print Pokemon
+function printPokemonDetails(pokemonList) {
+    //loop to print pokemon out    
+    for (var i = 0; i < pokemonList.length; i++) { 
+        if (pokemonList[i].height < 0.7){
+            document.write(pokemonList[i].name +' '+ '(height:' + pokemonList[i].height +')' +' '+'type:'+' ' + pokemonList[i].type + '<br>');
+        } else {
+            document.write(pokemonList[i].name +' '+ '(height:' + pokemonList[i].height +') -Wow, that\'s big! '+'type:'+' ' + pokemonList[i].type + '<br>');
+        }
     }
 }
+//calling printPokemonDetails function twice
+printPokemonDetails(repository);
+printPokemonDetails(repository2);
