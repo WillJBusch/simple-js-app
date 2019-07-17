b741334f979b1698472938760c0796dce64fd139
WillJBusch <50707325+WillJBusch@users.noreply.github.com>
2019-07-17 17:46:01 +0200

    changed/scripts.js
    changed from printing pokemondetails with for-loop to forEach-loop

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,22 @@ var repository2 = [
     {name: 'Hornliu', height: 0.3, type: ['Käfer, Gift']},
     {name: 'Taubsi', height: 0.3, type: ['Normal, Flug']}
 ]
-//declaring function of repository to print Pokemon
-function printPokemonDetails(pokemonList) {
-    //loop to print pokemon out    
+//declaring function with 'whatPokemon' as parameter do refer to diffren repositories
+function printPokemon (whatPokemon){
+    //declaring function with forEach-loop to print Pokemondetails
+    whatPokemon.forEach(function(pokemonDetails){
+        if (pokemonDetails.height < 0.7){
+            document.write(pokemonDetails.name +' '+ '(height:' + pokemonDetails.height +')' +' '+'type:'+' ' + pokemonDetails.type + '<br>');
+        } else {
+            document.write(pokemonDetails.name +' '+ '(height:' + pokemonDetails.height +') -Wow, that\'s big! '+'type:'+' ' + pokemonDetails.type + '<br>');
+        }    
+    });
+}
+//calling printPokemon function twice with diffrent repositories
+printPokemon(repository);
+printPokemon(repository2);
+/*function printPokemonDetails(pokemonList) {
+     for-loop to print pokemon out    
     for (var i = 0; i < pokemonList.length; i++) { 
         if (pokemonList[i].height < 0.7){
             document.write(pokemonList[i].name +' '+ '(height:' + pokemonList[i].height +')' +' '+'type:'+' ' + pokemonList[i].type + '<br>');
@@ -19,7 +32,8 @@ function printPokemonDetails(pokemonList) {
             document.write(pokemonList[i].name +' '+ '(height:' + pokemonList[i].height +') -Wow, that\'s big! '+'type:'+' ' + pokemonList[i].type + '<br>');
         }
     }
-}
-//calling printPokemonDetails function twice
+
+}*/
+/*calling printPokemonDetails function twice with diffrent repositories
 printPokemonDetails(repository);
-printPokemonDetails(repository2);
+printPokemonDetails(repository2);*/
