1723b8f51342baed4bef405dbe8f4ce38c1aa30e
WillJBusch <50707325+WillJBusch@users.noreply.github.com>
2019-10-14 14:30:45 +0200

    changed/scripts.js
    removed addListItem from pokemonRepository IIFE to be able to output pokemon names on console per click

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-
-
-/*(function () {
-    //Pokemon array
-    var repository = [ 
-        {name: 'Bisasam', height: 0.7, type: ['Pflanze', 'Gift']},
-        {name: 'Glumanda', height: 0.6, type: ['Feuer']},
-        {name: 'Schiggy', height: 0.5, type: ['Wasser']}
-    ];
-})*/
-    
-
 var repository2 = [
     {name: 'Raupy', height: 0.3, type: ['Käfer']},       
     {name: 'Hornliu', height: 0.3, type: ['Käfer, Gift']},
@@ -24,55 +12,55 @@ var repository2 = [
             {name: 'Glumanda', height: 0.6, type: ['Feuer']},
             {name: 'Schiggy', height: 0.5, type: ['Wasser']}
         ];
-        
+  
         function add(pokemon) {
             repository.push(pokemon);
         }
-        //declare function that creates buttons and li's 
-        function addListItem (pokemon) {
-            var listItem = document.createElement('li'); //create li                       
-            var button = document.createElement('button');
-            button.innerText = pokemon;
-            button.classList.add('buttonToStyle');            
-            listItem.appendChild(button);
-            $pokemonList.appendChild(listItem);          // add li to ul                        
-            button.addEventListener('click', function(event){                
-                showDetails(repository);
-                
-            });
-        }
-        //adding event listener
-        function showDetails(pokemon) {
-            pokemon.forEach(function(pokemon) {
-                console.log(pokemon.name)
-            });
-        }
-
+  
         function getAll() {
             return repository;
         }
         //returning functions inside pokemonRepository to be able reference outside of IIFE
-        return {                       
+        return {
+  
             add: add,
-            getAll: getAll,  
-            addListItem : addListItem          
+            getAll: getAll,
+  
+            //addListItem : addListItem  
         };
-    })();    
+    })();
+    //declare function that creates buttons and li's
+    function addListItem (pokemon) {
+        var listItem = document.createElement('li'); //create li  
+        var button = document.createElement('button'); //create button
+        button.innerText = pokemon.name;
+        button.classList.add('buttonToStyle');
+  
+        listItem.appendChild(button);
+        $pokemonList.appendChild(listItem); // add li to ul
+        //adding event listener
+        button.addEventListener('click', function(event){
+            showDetails(pokemon);
+        }); 
+  
+    }
+    
+    function showDetails(pokemon) {
+      console.log(pokemon.name)
+    }
     // referencing HTML ul-tag pokemon-list
     var $pokemonList = document.querySelector('.pokemon-list');
     //declaring function printPokemon
-    function printPokemon (){
-        //declaring function with forEach-loop to print Pokemondetails
-        pokemonRepository.getAll().forEach(function(pokemonDetails){            
-            
-            pokemonRepository.addListItem(pokemonDetails.name);  
-
+    function printPokemon(){
+        //declaring forEach function to print pokemon
+        pokemonRepository.getAll().forEach(function(pokemonDetails) {
+            addListItem(pokemonDetails)
         });
-    }    
+    }
     repository2.forEach(pokemonRepository.add) //referencing all repository2 items via forEach loop
     console.log(pokemonRepository.getAll()); // [ output Pokemon parameter in console ]
     printPokemon();//calling printPokemon function
-})()
+})();
 
 
 
